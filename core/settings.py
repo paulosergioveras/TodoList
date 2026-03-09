b36020cc84d97360a389995236b7d8611ee0cefc
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2026-03-08 22:18:39 -0300

    feat: use dbsqlite

--- core/settings.py
+++ core/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todolist',
-        'USER': 'postgres',
-        'PASSWORD': 'engele',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
